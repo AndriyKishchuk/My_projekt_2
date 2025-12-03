a12d334e55b5e145116431a3dd7a86840c3b20b6
w72228 <andriy302007@gmail.com>
2025-12-03 20:32:19 +0100

    add

--- Projekt_1/Program.cs
+++ Projekt_1/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string? name;
+        string? product;
+        decimal price;
+        string? adreess;
+        int adressNumber;
+        for (int i = 1; i <= 3; i++)
+        {
+
+            Console.WriteLine("Enter name of client:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter product:");
+            product = Console.ReadLine();
+
+            Console.WriteLine("Enter price: ");
+            price = Convert.ToDecimal(Console.ReadLine());
+
+            Console.WriteLine("Enter adress:");
+            adreess = Console.ReadLine();
+            Console.WriteLine("Enter adressNumber:");
+            adressNumber = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"Order No {i}");
+            Console.WriteLine($"Client: {name}");
+            Console.WriteLine($"Product: {product}, price {price} EUR.");
+            Console.WriteLine($"Adress:{adreess}, {adressNumber}.");
+            Console.WriteLine();
+
+
+
+
+        }
+    }
+}
